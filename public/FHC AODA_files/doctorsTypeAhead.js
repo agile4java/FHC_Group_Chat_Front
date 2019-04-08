79e263d43d2b8335de3b1519b9c2ead5c090cfa4
Chad Conway <agile4java@gmail.com>
2019-04-08 16:50:39 -0500

    initial

---
+++ public/FHC AODA_files/doctorsTypeAhead.js
@@ -0,0 +1,186 @@
+﻿    function compare(a,b) {
+        if (a.label.toLowerCase() < b.label.toLowerCase())
+            return -1;
+        if (a.label.toLowerCase() > b.label.toLowerCase())
+            return 1; 
+        return 0;
+    }  
+
+        
+    var termsArrayDoc;
+    var listURL = "/SiteAssets/DoctorsTypeAheadList.txt";
+    $.get(listURL, function(usersGet) {
+        termsArrayDoc = eval(usersGet); termsArrayDoc.sort(compare); });
+    var path = "/doctors/search/";
+    var gotSearchDoc = false;
+    
+
+    var displayListDoc = new Array();
+    var subItem = null;
+    var textValue = null;
+    var displayItem = null;    
+
+	  
+    //choose which item will be displayed
+    function appendToListDoc(){
+        displayListDoc.length = 0;
+        textValue = $("#docSearchInput").val();
+
+        textValue = textValue.replace("Dr.","");
+        textValue = textValue.replace("Dr ","");
+        textValue = textValue.replace("dr.","");
+        textValue = textValue.replace("dr ","");
+        textValue = textValue.replace("Doctor","");
+        textValue = textValue.replace("doctor","");
+        textValue = textValue.replace("(","\\(");
+        textValue = textValue.replace(")","\\)");
+        textValue = $.trim(textValue);
+		
+        var reg = new RegExp("(^"+textValue+")|(\\W"+textValue+")","i");
+		
+        for(var i=0;i<termsArrayDoc.length;i++){
+		
+            var item = termsArrayDoc[i].label;
+			
+            if(reg.test(item)){
+                displayListDoc.push(termsArrayDoc[i]);
+            }
+        }
+        return displayListDoc;
+    }
+
+    //format the displayed item
+    function formatListDoc(item){
+        displayItem = "<a href='#'>";
+        textValue = $("#docSearchInput").val();
+        textValueOld = textValue;
+        textValueOld = textValueOld.replace("Dr.","");
+        textValueOld = textValueOld.replace("Dr ","");
+        textValueOld = textValueOld.replace("dr.","");
+        textValueOld = textValueOld.replace("dr ","");
+        textValueOld = textValueOld.replace("Doctor","");
+        textValueOld = textValueOld.replace("doctor","");
+        textValueOld = $.trim(textValueOld);
+        var label= item.label;
+		
+        textValue = textValue.replace("Dr.","");
+        textValue = textValue.replace("Dr ","");
+        textValue = textValue.replace("dr.","");
+        textValue = textValue.replace("dr ","");
+        textValue = textValue.replace("Doctor","");
+        textValue = textValue.replace("doctor","");
+        textValue = textValue.replace("(","\\(");
+        textValue = textValue.replace(")","\\)");
+        textValue = $.trim(textValue);
+		
+        var reg = new RegExp("(^"+textValue+")|(\\W"+textValue+")","i");
+        var index = label.search(reg);
+		
+        if(index>0){
+            displayItem+=label.substring(0, index+1);
+            displayItem+="<font style='font-weight:bolder;'>"+label.substring(index+1,index+1+textValueOld.length)+"</font>";
+            displayItem+=label.substring(index+1+textValueOld.length,label.length);
+        }
+        else{
+            displayItem += label.replace(reg, "<font style='font-weight:bolder;'>"+
+							label.substring(0,textValueOld.length)+"</font>");
+        }
+        displayItem +="</a>";
+		
+        return displayItem;
+    } 
+    
+    $(function () {
+        $("#docSearchInput").autocomplete({
+            source: function(request, response){
+                response(appendToListDoc());
+            },
+            minLength:3,
+            select: function( event, ui ) {
+                var query = ui.item.url.split("?"); 
+                var url = ui.item.url;
+                if ( url.indexOf("k='") != -1)
+                {
+                    url = url.replace("k='", 'k="').replace("'", '"');
+                }
+               else if ( url.indexOf('k="') != -1)
+               {
+               }
+               else
+               { 
+                  if ( url.indexOf("k=") != -1 )
+                  {
+                     url = url.replace("k=", 'k=\u0022');
+                     url = url + '\u0022';
+                   }
+               }
+
+                if(query.length == 1) {
+                    url += "?ek=" + ui.item.label; 
+                } else {
+                    url += "&ek=" + ui.item.label;
+                }
+                if (!gotSearchDoc) {
+                    window.location.href = url;
+                    gotSearchDoc = true;
+                }                                                   
+            }
+        })
+        .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+            return $( "<li>" )
+              .append(formatListDoc(item))
+              .appendTo( ul );
+        };
+    });
+    
+    
+    /* keyup was not working due to event listener conflicts 
+$(document).on('keyup', '#docSearchInput', function (event) { 
+});*/
+        $('#docSearchInput').keydown(function (e) {
+           if (e.keyCode == 13 && $('#docSearchInput').val() !== '') {        
+              var term = $('#docSearchInput').val();
+              for(var i = 0; i < termsArrayDoc.length; i++){
+                 if (term == termsArrayDoc[i].label)
+                 {
+                    var url = termsArrayDoc[i].url;
+					var query = url.split("?");
+					
+					if ( url.indexOf("k='") != -1)
+					{
+					   url = url.replace("k='", 'k="').replace("'", '"');
+					}
+					else if ( (url.indexOf("Center") != -1) || (url.indexOf("Dental") != -1) || (url.indexOf("Therapy") != -1) || (url.indexOf("Plaza") != -1) || (url.indexOf("Central") != -1))
+					{ 
+					   url = url.replace("k=", 'k="');
+					   url = url + '"';
+					}
+					    
+					if(query.length == 1) {
+					    url += "?ek=" + term; 
+					} else {
+					   url += "&ek=" + term;
+					}
+					if (!gotSearchDoc) {
+					    window.location.href = url;
+					    gotSearchDoc = true;
+					}
+					return true;
+                 }
+              }
+              if (!gotSearchDoc) {
+                 if ( ( $('#docSearchInput').val().indexOf('Center') != -1) || ( $('#docSearchInput').val().indexOf('Dental') != -1) || ( $('#docSearchInput').val().indexOf('docSearchInput') != -1) || ( $('#docSearchInput').val().indexOf('Plaza') != -1) || ( $('#docSearchInput').val().indexOf('Central') != -1) )
+                 {
+                    window.location.href = '/doctors/search/?k=' + '?k="' + $('#docSearchInput').val().replace(/\-/g,' ').replace('&',' ').replace('＆',' ').replace('   ',' ').replace('  ',' ') + '"';
+                 }
+                 else 
+                 {
+                    window.location.href = '/doctors/search/?k=' + '?k=\u0022' + $('#docSearchInput').val().replace(/\-/g,' ').replace('&',' ').replace('＆',' ').replace('   ',' ').replace('  ',' ') + '\u0022';
+                 }
+                 gotSearchDoc = true;
+              }      
+           }        
+        });
+        
+
+
---
+++ public/FHC AODA_files/newGlobalHeader.js
@@ -0,0 +1,149 @@
+﻿  $(document).ready(function () {   
+    $('.nav.mobile-nav').on('click', function () {
+        //$('.mobile-header, .all-the-header, .container-hider').toggleClass('open');
+        $('.mobile-header, .all-the-header, .container-hider').toggleClass('open').promise().done(function () {
+            if ($("#mobileHeader").hasClass("open")) {
+                console.log('send', 'event', 'global nav link', 'click', 'hamburger');
+                ga('send', 'event', 'global nav link', 'click', 'hamburger', 0);
+            }
+        });
+        $('.mobile-quick-tasks').toggleClass('open');
+        $('body').toggleClass('noScroll');
+        //closeTypeAhead();
+    });    
+    
+    
+	var s_pageName = $(location).attr('pathname');
+	
+	if (s_pageName === '/') {
+		s_pageName = 'marshfieldclinic|home|index';  // if we are at the root, set the pageName to this value
+	};
+    
+    $('html').click(function () {
+        $('.top-level-nav > li.hasKids.active').removeClass('active');
+        //menuscroll
+        //$('body').removeClass('noScroll');
+        $('.all-the-header').removeClass('openDesktop');
+        $('.container-hider').removeClass('openDesktop');
+    });
+    
+
+    $('.top-level-nav').click(function (event) {
+        event.stopPropagation();
+    });
+    $('.top-level-nav > li.hasKids > a').click(function (event) {
+        var $clickedLi = $(this);
+        $clickedLi.parent().siblings().removeClass('active');
+        if ($clickedLi.parent().hasClass('active')) {
+            $clickedLi.parent().removeClass('active');
+            //menuscroll
+            //$('body').removeClass('noScroll');
+            $('.all-the-header').removeClass('openDesktop');
+            $('.container-hider').removeClass('openDesktop');            
+        } else {
+            $clickedLi.parent().addClass('active');
+            //menuscroll
+            //$('body').addClass('noScroll');
+            $('.all-the-header').addClass('openDesktop');
+            $('.container-hider').addClass('openDesktop');           
+        }
+    });
+    $('.dir.locations > a.heading').on('click', function(){
+        if(!$(this).hasClass('active')){
+            $('a.heading').removeClass('active');
+            $(this).addClass('active');
+            $(this).parent().children('ul').addClass('active');
+            
+        }else{
+            $(this).removeClass('active');
+            $('body').find('ul').removeClass('active');
+        }
+
+    });    
+    
+  
+    $('.header-test-button').click(function (event) {
+        event.preventDefault();
+         if($('#newSearchInput').val() !== ''){             
+             // send to search 
+             window.location = '/Search/?k=' + $('#newSearchInput').val(); 
+         } 
+
+    });
+        
+
+        $('#newSearchInput').keydown(function (e) {
+        if (e.keyCode == 13 && $('#newSearchInput').val() !== '') {
+        
+            var searchTerm = $('#newSearchInput').val();
+            window.location.href = '/Search/?k=' + searchTerm;
+            return false;
+        }
+        else {
+            return true;
+        }
+        });
+
+       $('.top-level-nav > li.hasKids > ul > li > div > button').click(function (event) { 
+        event.preventDefault();
+         if($('#docSearchInput').val() !== ''){ 
+             // send to search 
+             window.location = '/doctors/search/?k=' + $('#docSearchInput').val(); 
+         } 
+        });
+
+/* keyup was not working due to event listener conflicts 
+$(document).on('keyup', '#docSearchInput', function (event) { 
+});*/
+/* AJK 03/09/2018 -- this logic was moved to doctorsTypeAhead.js
+       $('#docSearchInput').keydown(function (e) {
+        if (e.keyCode == 13 && $('#docSearchInput').val() !== '') {
+            
+                     
+            var searchTerm = $('#docSearchInput').val();
+            window.location.href = '/doctors/search/?k=' + searchTerm;
+            return false;
+        }
+        else {
+            return true;
+        }
+        });*/
+        
+     $('#newSearchInput').click(function(){ 
+         $(this).val(''); 
+     }); 
+
+     $('#docSearchInput').click(function(){ 
+         $(this).val(''); 
+     }); 
+        
+    $('.top-level-nav > li.hasKids > ul.explore > li > ul > li').click(function (event) {
+        window.location=$(this).find("a").attr("href"); 
+        return false;
+    });
+    
+    function evaluateWindow(){
+        var body = $('body');
+        if (window.innerWidth < 998) {
+            $('#globalNav-doctorsButton').attr('href', '/doctors');
+            body.addClass('mobile');
+            body.removeClass('desktop');
+            $('.mobile-header').show();
+        } else {
+            $('#globalNav-doctorsButton').attr('href', '#');
+            body.removeClass('mobile');
+            body.addClass('desktop');
+            $('.mobile-header').hide();
+        }
+    }    
+    
+    $(window).resize(function () {
+        evaluateWindow();
+    });    
+    evaluateWindow(); 
+
+});
+
+ 
+
+
